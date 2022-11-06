e0a77ef51a838392cafdcef639b1078abcf6de6f
Teppei Kikuchi <>
2022-11-06 14:58:43 +0700

    update admob id

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -4,10 +4,10 @@ class Config {
   static const dictionarySpreadSheetName = '001_myanmar_dictionary';
   static const directoryName = 'PROD';
   static const dbName = 'app_database.db';
-  static const adUnitIdAndroidBanner = kDebugMode ? 'ca-app-pub-3940256099942544/6300978111' : 'ca-app-pub-3388807447141549/1005593741';
-  static const adUnitIdIosBanner = kDebugMode ? 'ca-app-pub-3940256099942544/2934735716' : 'ca-app-pub-3388807447141549/9239306980';
-  static const adUnitIdAndroidInterstitial = kDebugMode ? 'ca-app-pub-3940256099942544/8691691433' : 'ca-app-pub-3388807447141549/1743960348';
-  static const adUnitIdIosInterstitial = kDebugMode ? 'ca-app-pub-3940256099942544/5135589807' : 'ca-app-pub-3388807447141549/1360816962';
+  static const adUnitIdAndroidBanner = kDebugMode ? 'ca-app-pub-3940256099942544/6300978111' : 'ca-app-pub-9974265212679212/3788562658';
+  static const adUnitIdIosBanner = kDebugMode ? 'ca-app-pub-3940256099942544/2934735716' : 'ca-app-pub-9974265212679212~8869498946';
+  static const adUnitIdAndroidInterstitial = kDebugMode ? 'ca-app-pub-3940256099942544/8691691433' : 'ca-app-pub-9974265212679212/9292132786';
+  static const adUnitIdIosInterstitial = kDebugMode ? 'ca-app-pub-3940256099942544/5135589807' : 'ca-app-pub-9974265212679212~8869498946';
 
   static const apiKeyHeader = "x-api-key";
   static const contentTypeHeader = "Content-type";
